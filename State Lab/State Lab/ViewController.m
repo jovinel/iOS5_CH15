c3b758f97f6785e533dfde2e160db568af0be769
crescens <crescens.techstars@gmail.com>
2013-12-09 14:24:06 +0800

    Finished a demo for switching from Active to Inactive and back state transitions
    Signed-off-by: crescens <crescens.techstars@gmail.com>

--- State Lab/State Lab/ViewController.m
+++ State Lab/State Lab/ViewController.m
@@ -12,18 +12,82 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    BOOL animate;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    CGRect bounds = self.view.bounds;
+    CGRect labelFrame = CGRectMake(bounds.origin.x, CGRectGetMidY(bounds) - 50, bounds.size.width, 100);
+    self.label = [[UILabel alloc] initWithFrame:labelFrame];
+    self.label.font = [UIFont fontWithName:@"Helvetica" size:70];
+    self.label.text = @"Bazinga!";
+    self.label.textAlignment = NSTextAlignmentCenter;
+    self.label.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.label];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)rotateLabelDown
+{
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         self.label.transform = CGAffineTransformMakeRotation(M_PI);
+                     }
+                     completion:^(BOOL finished){
+                         [self rotateLabelUp];
+                     }];
+}
+
+- (void)rotateLabelUp
+{
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         self.label.transform = CGAffineTransformMakeRotation(0);
+                     }
+                     completion:^(BOOL finished){
+                         if (animate) {
+                             [self rotateLabelDown];
+                         }
+                     }];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+        [center addObserver:self
+                   selector:@selector(applicationWilLResignActive)
+                       name:UIApplicationWillResignActiveNotification
+                     object:nil];
+        [center addObserver:self
+                   selector:@selector(applicationDidBecomeActive)
+                       name:UIApplicationDidBecomeActiveNotification
+                     object:nil];
+    }
+    return self;
+}
+
+- (void)applicationWillResignActive
+{
+    NSLog(@"VC: %@", NSStringFromSelector(_cmd));
+    animate = NO;
+}
+- (void)applicationDidBecomeActive
+{
+    NSLog(@"VC: %@", NSStringFromSelector(_cmd));
+    animate = YES;
+    [self rotateLabelDown];
 }
 
 @end
